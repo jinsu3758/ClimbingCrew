3905c17efa08b957e1fa87a4d85122a97ba6f75a
jinsu3758 <jinsu3758@naver.com>
2020-09-14 19:38:22 +0900

    init

---
+++ ClimbFriendsTests/ClimbFriendsTests.swift
@@ -0,0 +1,34 @@
+//
+//  ClimbFriendsTests.swift
+//  ClimbFriendsTests
+//
+//  Created by 박진수 on 2020/09/14.
+//  Copyright © 2020 jinsu. All rights reserved.
+//
+
+import XCTest
+@testable import ClimbFriends
+
+class ClimbFriendsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
